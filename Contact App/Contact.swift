8d2618535101b59239379d2dc35bb590b37922e5
DARSHIL KADIWALA <88822877+darshilkadiwala@users.noreply.github.com>
2022-04-13 15:29:47 +0530

    Delete Contact App directory

--- Contact App/Contact.swift
+++
@@ -1,9 +0,0 @@
-import CoreData
-
-@objc(Contact)
-class Contact: NSManagedObject{
-    @NSManaged var id: NSNumber!
-    @NSManaged var name: String!
-    @NSManaged var phoneNumber: String!
-    @NSManaged var dob: Date!
-}
